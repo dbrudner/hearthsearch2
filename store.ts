0fd8a7b64c54bda03cc4d3ee84e5cc4a81e45c0b
dbrudner <dbrudne1@gmail.com>
2018-08-30 21:47:15 -0500

    build deck reducer

--- store.ts
+++ store.ts
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { searchReducer } from "./features/search/reducer";
+import { buildDeckReducer } from "./features/build-deck/reducer";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-	search: searchReducer
+	search: searchReducer,
+	deck: buildDeckReducer
 });
 
 export function initializeStore() {

4acf5c864594b75d4d961332cc66db3c517223d0
dbrudner <dbrudne1@gmail.com>
2018-08-30 22:55:55 -0500

    reducer stuff

--- features/build-deck/reducer.ts
+++ features/build-deck/reducer.ts
@@ -3,11 +3,20 @@ export const CARD_REMOVED = "CARD_REMOVED";
 
 export const buildDeckReducer = (state = [], action) => {
 	if (action.type === CARD_ADDED) {
-		return [...state, action.payload];
+		if (state.includes(action.payload.name)) {
+			return {...state, [action.payload.name]: {...action.payload, quantity: 2}};
+		} else {
+			return {...state, [action.payload.name]: {...action.payload, quantity: 1}};
+		}
 	}
 
 	if (action.type === CARD_REMOVED) {
-		return state.filter(card => card.name !== action.payload);
+		if (state.includes(action.payload.name)) {
+			return {...state, [action.payload.name]: {...action.payload, quantity: 1}};
+		} else {
+			const {action.payload.name, ...deck} = state
+			return deck
+		}
 	}
 
 	return state;
